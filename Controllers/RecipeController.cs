4b252a48ebd88ae604e156017dc6c128e9509f25
Mateusz Leśko <mateusz.lesko@outlook.com>
2021-06-16 00:02:56 +0200

    Recipe Controller View

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using beerOfThings.Models;
+
+namespace beerOfThings.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly beerOfThingsContext _context;
+
+        public RecipeController(beerOfThingsContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            List<Recipe> recipes = await _context.Recipes.ToListAsync();
+            return View(recipes);
+        }
+    }
+}
